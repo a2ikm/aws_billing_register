4a6bba1879b4b7c068b3091ad0f2874b1a4cecb4
Masato Ikeda <masato.ikeda@gmail.com>
2013-12-31 00:46:11 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwsBillingRegister::Application.config.secret_key_base = '761a5784f0d35d0a0af5a0e6999b8ba5617fb783946d9c3944e0eac201dffc18165822617cbc067aea135c42e46f1cb23a8be3ffa449af6a8b5573eac7fd6a6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AwsBillingRegister::Application.config.session_store :cookie_store, key: '_aws_billing_register_session'
