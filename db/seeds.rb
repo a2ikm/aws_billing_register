3927bdc0e844accfc1019be51826d16c10a7dc5a
Masato Ikeda <masato.ikeda@gmail.com>
2014-01-03 05:57:01 +0900

    Seed fixtures from db/fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# coding: utf-8
+require "active_record/fixtures"
+
+FIXTURES_DIR = File.expand_path("../fixtures", __FILE__)
+
+fixtures =
+  if ENV["FIXTURES"]
+    ENV["FIXTURES"].split(",").map do |f|
+      f = "#{f}.yml" unless f =~ /\.yml$/
+      File.expand_path(f, FIXTURES_DIR)
+    end
+  else
+    Dir.glob("#{FIXTURES_DIR}/*.yml")
+  end
+
+
+fixtures.each do |yml_path|
+  puts "Loading #{yml_path.sub("#{FIXTURES_DIR}/", "")}"
+
+  yml_name = File.basename(yml_path, ".yml")
+  yml_class = yml_name.classify
+
+  conn = ActiveRecord::Base.connection
+  fixtures = ActiveRecord::FixtureSet.new(conn, yml_name, yml_class, "#{File.dirname(yml_path)}/#{yml_name}")
+  fixtures.table_rows.each do |table_name, rows|
+    rows.each do |row|
+      conn.delete "DELETE FROM #{table_name} WHERE id = #{row["id"]}"
+      conn.insert_fixture row, table_name
+    end
+  end
+end
+
